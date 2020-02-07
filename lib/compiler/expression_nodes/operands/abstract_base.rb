974d0a3be949dbb75cd75aa976a1cd75e39a393c
Koji Onishi <fursich0@gmail.com>
2020-02-08 01:54:15 +0900

    roughly outline segment nodes

--- lib/compiler/expression_nodes/operands/abstract_base.rb
+++ lib/compiler/expression_nodes/operands/abstract_base.rb
@@ -21,28 +21,13 @@ module Expression
 
         REGISTER_ASSIGNMENT = {
           'sp'       => 'SP',
-          'local'    => 'LCL',
-          'argument' => 'ARG',
-          'this'     => 'THIS',
-          'that'     => 'THAT',
         }.merge( 13.upto(15).map { |idx| ["r#{idx}", "R#{idx}"] }.to_h )
-
-        REGISTER_INDEX = {
-          'pointer'  => 3,
-          'temp'     => 5,
-        }
 
         def resolve(memory_segment, index=nil)
           raise NotImplementedError
           REGISTER_ASSIGNMENT[memory_segment]
           reg_idx = REGISTER_INDEX[memory_segment] + index
           "R#{reg_idx}"
-        end
-
-        def increment_a(by:)
-          (<<~"ASSEMBLY" * by).chomp
-            A = A + 1
-          ASSEMBLY
         end
       end
 
@@ -74,27 +59,34 @@ module Expression
 
         def access_direct(index) # pointer/temp
           <<~"ASSEMBLY".chomp
-            @#{resolve(index)}
+            @R#{resolve(index)}
           ASSEMBLY
         end
 
         def resolve(index)
-          reg_idx = REGISTER_INDEX[@name] + index
-          "R#{reg_idx}"
+          base_index + index.to_i
         end
       end
 
       class IndirectReference < SegmentBase
-        def load(index) # lcl/arg/this/that
+        def load(index)
           <<~"ASSEMBLY".chomp
             #{access_indirect(index)}
             D = M
           ASSEMBLY
         end
 
-        def store(index) # lcl/arg/this/that
+        def before_pop
           <<~"ASSEMBLY".chomp
             #{access_indirect(index)}
+            @R13
+            M = D
+          ASSEMBLY
+        end
+
+        def store(index) # lcl/arg/this/that
+          <<~"ASSEMBLY".chomp
+            @R13
             M = D
           ASSEMBLY
         end
@@ -104,13 +96,14 @@ module Expression
         def access_indirect(index) # lcl/arg/this/that
           <<~"ASSEMBLY".chomp
             @#{resolve}
-            A = M
-            #{increment_a(by: index)}
+            D = M
+            @#{index}
+            AD = D + A
           ASSEMBLY
         end
 
         def resolve
-          REGISTER_ASSIGNMENT[@name]
+          base_register
         end
       end
     end
--- lib/compiler/expression_nodes/operands/segment.rb
+++ lib/compiler/expression_nodes/operands/segment.rb
@@ -1,16 +1,47 @@
 module Expression
   module Node
     module MemorySegment
-      class Constant < ImmediateValue; end
-      class Local    < IndirectReference; end
-      class Argument < IndirectReference; end
+      class Constant < ImmediateValue
+      end
 
-      class This     < IndirectReference; end
-      class That     < IndirectReference; end
-      class Pointer  < DirectReference; end
+      class Local < IndirectReference
+        def base_register
+          'LCL'
+        end
+      end
 
-      class Temp     < DirectReference; end
+      class Argument < IndirectReference
+        def base_register
+          'ARG'
+        end
+      end
+
+      class This < IndirectReference
+        def base_register
+          'THIS'
+        end
+      end
+
+      class That < IndirectReference
+        def base_register
+          'THAT'
+        end
+      end
+
+      class Pointer < DirectReference
+        def base_index
+          3
+        end
+      end
+
+      class Temp < DirectReference
+        def base_index
+          5
+        end
+      end
+
       class Static   < DirectReference
+        # FIXME
         def resolve(index)
           "#FILENAME#.#{index}"
         end
